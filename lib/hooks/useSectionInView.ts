13b96b3b64e7f5deba2b478f429040a750357e53
Pascual Eburi <pascualeburi@gmail.com>
2023-10-17 10:33:13 +0200

    Create & Implement useSectionInView hook

---
+++ lib/hooks/useSectionInView.ts
@@ -0,0 +1,22 @@
+/* eslint-disable react-hooks/exhaustive-deps */
+import { useActiveSectionContext } from "@/context/activeSectionContext";
+import { useEffect } from "react";
+import { useInView } from "react-intersection-observer";
+import type { useSectionInViewProps } from "../types";
+
+export default function useSectionInView ({
+    section, threshold = 0.5
+}:useSectionInViewProps) {
+      // check when component is in view to set active class to his link
+  const { ref, inView } = useInView({ threshold });
+  const { setActiveSection , timeOfLastClick} = useActiveSectionContext();
+  useEffect(() => {
+    if (inView && Date.now() - timeOfLastClick > 1000 /* 1ms */) {
+      setActiveSection(section);
+    }
+  }, [inView, timeOfLastClick, section]);
+
+  return {
+    ref
+  }
+}
