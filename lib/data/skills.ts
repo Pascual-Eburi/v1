b4c9ef20c47026dc7e0e58193c39a4981cd2bdbc
Pascual Eburi <pascualeburi@gmail.com>
2023-10-18 23:18:07 +0200

    Add skill data

--- lib/data/skills.ts
+++ lib/data/skills.ts
@@ -1,23 +1,47 @@
 // skills
 
-export const skillsData = [
-    "HTML",
-    "CSS",
-    "JavaScript",
-    "TypeScript",
-    "React",
-    "Next.js",
-    "Node.js",
-    "Git",
-    "Tailwind",
-    "Prisma",
-    "MongoDB",
-    "Redux",
-    "GraphQL",
-    "Apollo",
-    "Express",
-    "PostgreSQL",
-    "Python",
-    "Django",
-    "Framer Motion",
-  ] as const;
+import type { Skills } from "../types";
+
+import boostrap from "@/components/icons/bootstrap.svg";
+import bash from "@/components/icons/bash.svg";
+
+  export const skills: Skills = {
+    'Front-End': [
+      {name: 'HTML', image: ''},
+      {name: 'CSS', image: ''},
+      {name: 'JavaScript', image: ''},
+      {name: 'Bootstrap', image: ''},
+      {name: 'JQuery', image: ''},
+      {name: 'Tailwind', image: ''},
+      {name: 'React Js', image: ''},
+      {name: 'Next Js', image: ''},
+    ],
+    'Back-End': [
+      {name: 'PHP', image: ''},
+      {name: 'Python', image: ''},
+      {name: 'Laravel', image: ''},
+      {name: 'CodeIgniter', image: ''},
+      {name: 'Django', image: ''},
+      {name: 'MySQL', image: ''},
+      {name: 'PostreSQL', image: ''},
+      {name: 'Oracle', image: ''},
+    ],
+    'Testing': [
+      {name: 'PHPUnit', image: ''},
+      {name: 'Pest', image: ''},
+      {name: 'Jest', image: ''},
+      {name: 'Cypress', image: ''},
+      {name: 'PyTest', image: ''},
+      {name: 'Laravel Dusk', image: ''},
+    ],
+    'Others': [
+      {name: 'Git', image: ''},
+      {name: 'GitHub', image: ''},
+      {name: 'Jira', image: ''},
+      {name: 'Docker', image: ''},
+      {name: 'Postman', image: ''},
+      {name: 'Wordpress', image: ''},
+      {name: 'Bash', image: ''},
+    ]
+
+  }
