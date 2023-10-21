45f4cafe46f57e63e5c53c7aed02dcd16ec85cc3
Pascual Eburi <pascualeburi@gmail.com>
2023-10-21 18:11:58 +0200

    Submit btn componenent

---
+++ components/submit-btn.tsx
@@ -0,0 +1,25 @@
+
+import { usePending } from '@/context/pendingContex';
+import { FaPaperPlane } from 'react-icons/fa'
+
+
+export default function SubmitButton() {
+    const {pending} = usePending();
+    console.log(pending)
+  return (
+    <button
+    type="submit"
+    disabled={pending === true}
+    className="group flex items-center justify-center gap-2 h-[3rem] w-full sm:w-[12rem] bg-gray-900 text-white rounded-full outline-none transition-all focus:scale-110 hover:scale-110 hover:bg-gray-950 active:scale-105 dark:bg-white dark:bg-opacity-10 disabled:scale-100 disabled:bg-opacity-65"
+  >
+      {pending  === true ? (
+        <div className="h-5 w-5 animate-spin rounded-full border-b-2 border-white"></div>
+      ) : (
+        <>
+          Send Message{" "}
+          <FaPaperPlane className="text-xs opacity-70 transition-all group-hover:translate-x-1 group-hover:-translate-y-1" />{" "}
+        </>
+      )}
+  </button>
+  )
+}
