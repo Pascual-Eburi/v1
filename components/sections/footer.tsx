6e2acd1b4b438bb79052679567276573ef36ae11
Pascual Eburi <pascualeburi@gmail.com>
2023-10-22 20:26:32 +0200

    Define footer componenet

---
+++ components/sections/footer.tsx
@@ -0,0 +1,19 @@
+import {BsFillHeartFill} from "react-icons/bs"
+
+export default function Footer() {
+  const year = new Date().getFullYear();
+  return (
+    <footer className="mb-10 px-4 text-center text-gray-500">
+      <small className="mb-2 block text-xs">
+        &copy; { year} Pascual Eburi. All rights reserved.
+      </small>
+      <p className="text-xs text-center">
+        <span className="font-semibold">About this website:</span> 
+        <span className="flex"> 
+            Made with <span className="mx-2"><BsFillHeartFill color="#f87171"/> </span> 
+            built with React & Next.js (App Router & Server Actions), TypeScript, Tailwind CSS, Framer Motion, React Email & Resend, Vercel hosting.
+        </span>
+      </p>
+    </footer>
+  )
+}
