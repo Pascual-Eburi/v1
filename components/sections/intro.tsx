c0341c8cb970eccfddc7155ee246fc583fa2fdd9
Pascual Eburi <pascualeburi@gmail.com>
2023-10-23 20:38:29 +0200

    Dark mode

--- components/sections/intro.tsx
+++ components/sections/intro.tsx
@@ -64,11 +64,10 @@ export default function Intro() {
         initial={{ opacity: 0, y: 100 }}
         animate={{ opacity: 1, y: 0 }}
       >
-        <span className="font-bold">Hello, I'm Ricardo.</span> I'm a{" "}
-        <span className="font-bold">full-stack developer</span> with{" "}
-        <span className="font-bold">8 years</span> of experience. I enjoy
-        building <span className="italic">sites & apps</span>. My focus is{" "}
-        <span className="underline">React (Next.js)</span>.
+        <span className="font-bold">Hello, I'm Pascual.</span> I'm a{" "}
+        <span className="font-bold"> Web developer and SysAdmin </span>. I enjoy
+        building <span className="italic">things for the web</span>. My focus is{" "}
+        <span className="underline"> PHP / Python - React / Vue </span>.
       </motion.h1>
 
       <motion.div
@@ -100,21 +99,24 @@ export default function Intro() {
           <HiDownload className="opacity-60 group-hover:translate-y-1 transition" />
         </a>
 
-        <a
-          className="bg-white p-4 text-gray-700 hover:text-gray-950 flex items-center gap-2 rounded-full focus:scale-[1.15] hover:scale-[1.15] active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10 dark:text-white/60"
-          href="https://linkedin.com"
-          target="_blank"
-        >
-          <BsLinkedin />
-        </a>
+          <div className="flex items-center justify-center gap-2">
 
-        <a
-          className="bg-white p-4 text-gray-700 flex items-center gap-2 text-[1.35rem] rounded-full focus:scale-[1.15] hover:scale-[1.15] hover:text-gray-950 active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10 dark:text-white/60"
-          href="https://github.com"
-          target="_blank"
-        >
-          <FaGithubSquare />
-        </a>
+              <a
+                className="bg-white p-4 text-gray-700 hover:text-gray-950 flex items-center gap-2 rounded-full focus:scale-[1.15] hover:scale-[1.15] active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10 dark:text-white/60"
+                href="https://www.linkedin.com/in/pascualeburi/"
+                target="_blank"
+              >
+                <BsLinkedin />
+              </a>
+
+              <a
+                className="bg-white p-4 text-gray-700 flex items-center gap-2 text-[1.35rem] rounded-full focus:scale-[1.15] hover:scale-[1.15] hover:text-gray-950 active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10 dark:text-white/60"
+                href="https://github.com/Pascual-Eburi"
+                target="_blank"
+              >
+                <FaGithubSquare />
+              </a>
+          </div>
       </motion.div>
     </section>
   );
