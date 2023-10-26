dd9cf9ec03959e8704e540977e6143c2fbdb4ba8
Pascual Eburi <pascualeburi@gmail.com>
2023-10-26 23:28:41 +0200

    Updated experiences data

--- lib/data/experiences.ts
+++ lib/data/experiences.ts
@@ -5,27 +5,27 @@ import { LuGraduationCap } from "react-icons/lu";
 
 export const experiencesData = [
     {
-      title: "Graduated bootcamp",
-      location: "Miami, FL",
+      title: "System Administrator Intern",
+      location: "NTT DATA, Murcia - Spain",
       description:
-        "I graduated after 6 months of studying. I immediately found a job as a front-end developer.",
-      icon: React.createElement(LuGraduationCap),
-      date: "2019",
-    },
-    {
-      title: "Front-End Developer",
-      location: "Orlando, FL",
-      description:
-        "I worked as a front-end developer for 2 years in 1 job and 1 year in another job. I also upskilled to the full stack.",
+        "Manage and resolve or escalate incident tickets reported by customers. Prepare and send communications to customers to inform them about the resolution of their incident or complaint.Follow up on resolved tickets to ensure effective resolution and customer satisfaction..",
       icon: React.createElement(CgWorkAlt),
-      date: "2019 - 2021",
+      date: "March - June 2023",
     },
     {
-      title: "Full-Stack Developer",
-      location: "Houston, TX",
+      title: "Web Developer Freelancer",
+      location: "UPWORK, Remote",
       description:
-        "I'm now a full-stack developer working as a freelancer. My stack includes React, Next.js, TypeScript, Tailwind, Prisma and MongoDB. I'm open to full-time opportunities.",
-      icon: React.createElement(FaReact),
-      date: "2021 - present",
+        "I worked as a freelancer identifying and solving technical problems on existing websites improving their functionality. Designing, creating and maintaining online shops, increasing business sales. Resolving customer queries and problems effectively.",
+      icon: React.createElement(CgWorkAlt),
+      date: "October 2021 - March 2023",
+    },
+    {
+      title: "Administrative Assistant",
+      location: "BITNOVA S.L, Murcia - Spain",
+      description:
+        "Resolve issues with customers and suppliers, maintaining clear and friendly communication to ensure customer satisfaction.Supervise, organise and manage inventory, ensuring accurate stock control and optimising operating costs.",
+      icon: React.createElement(CgWorkAlt),
+      date: "March - June 2021",
     },
   ] as const;
