72aede872fb02c2d8746df08b49c7a4c054996b3
Pascual Eburi <pascualeburi@gmail.com>
2023-10-26 23:29:45 +0200

    Wip

--- lib/data/projects.ts
+++ lib/data/projects.ts
@@ -1,30 +1,30 @@
 
-import corpcommentImg from "@/public/img/projects/corpcomment.png";
-import rmtdevImg from "@/public/img/projects/rmtdev.png";
-import wordanalyticsImg from "@/public/img/projects/wordanalytics.png";
+import HomeAutomatricula from "@/public/img/projects/automatricula/home.png";
+import HomeApiAutomatricula from "@/public/img/projects/api-automatricula/api.png";
+import HomePharma from "@/public/img/projects/pharma/home.png";
 
 
 // projects data
 export const projectsData = [
     {
-      title: "CorpComment",
+      title: "Automatricula",
       description:
         "I worked as a full-stack developer on this startup project for 2 years. Users can give public feedback to companies.",
       tags: ["React", "Next.js", "MongoDB", "Tailwind", "Prisma"],
-      imageUrl: corpcommentImg,
+      imageUrl: HomeAutomatricula,
     },
     {
       title: "rmtDev",
       description:
         "Job board for remote developer jobs. I was the front-end developer. It has features like filtering, sorting and pagination.",
       tags: ["React", "TypeScript", "Next.js", "Tailwind", "Redux"],
-      imageUrl: rmtdevImg,
+      imageUrl: HomeApiAutomatricula,
     },
     {
       title: "Word Analytics",
       description:
         "A public web app for quick analytics on text. It shows word count, character count and social media post limits.",
       tags: ["React", "Next.js", "SQL", "Tailwind", "Framer"],
-      imageUrl: wordanalyticsImg,
+      imageUrl: HomePharma,
     },
   ] as const;
