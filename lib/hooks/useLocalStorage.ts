30c1ef223249e39f4ac239e43025bdcdee3779ac
Pascual Eburi <pascualeburi@gmail.com>
2023-10-26 23:10:57 +0200

    Local Storage hook

---
+++ lib/hooks/useLocalStorage.ts
@@ -0,0 +1,29 @@
+import { useState, useEffect } from 'react';
+import type { Theme } from '../types';
+
+export function useLocalStorage(key: string): [string | null, (value: Theme) => void, (key: string) => string | null] {
+  const [storedValue, setStoredValue] = useState<Theme | null>(null);
+
+  useEffect(() => {
+    if (typeof window !== 'undefined') {
+      const item = window.localStorage.getItem(key) as Theme | null;
+      
+      setStoredValue(item);
+    }
+  },  [key]);
+
+  const setItem = (value: Theme) => {
+    if (typeof window !== 'undefined') {
+      window.localStorage.setItem(key, value);
+      setStoredValue(value);
+    }
+  };
+
+  const getItem = (key: string) : Theme | null => {
+    return typeof window !== 'undefined' 
+    ? window.localStorage.getItem(key) as Theme 
+    :  null;
+  };
+
+  return [storedValue, setItem, getItem];
+}
