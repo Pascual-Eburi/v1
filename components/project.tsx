eb94f050b40d0d6f16332110704673dd976fc2a1
Pascual Eburi <pascualeburi@gmail.com>
2023-10-27 23:38:40 +0200

    Wip

--- components/project.tsx
+++ components/project.tsx
@@ -24,7 +24,7 @@ export default function Project ({ title, description, tags, imageUrl }: Project
       }} 
       className="group mb-3 sm:mb-8 last:mb-0"
     >
-      <section className="bg-gray-100 max-w-[42rem] border border-black/5 rounded-lg overflow-hidden sm:pr-8 relative sm:h-[20rem] hover:bg-gray-200 transition sm:group-even:pl-8 dark:text-white dark:bg-white/10 dark:hover:bg-white/20">
+      <section className="bg-gray-100 max-w-[42rem] border border-black/5 rounded-lg overflow-hidden sm:pr-8 relative  hover:bg-gray-200 transition sm:group-even:pl-8 dark:text-white dark:bg-white/10 dark:hover:bg-white/20">
         <div className="pt-4 pb-7 px-5 sm:pl-10 sm:pr-2 sm:pt-10 sm:max-w-[40%] flex flex-col h-full sm:group-even:ml-[22rem]">
           <h3 className="text-2xl font-semibold">{title}</h3>
           <p className="mt-2 leading-relaxed text-gray-700 dark:text-white/70">
@@ -46,7 +46,7 @@ export default function Project ({ title, description, tags, imageUrl }: Project
           src={imageUrl}
           alt="Project I worked on"
           quality={95}
-          className="absolute hidden sm:block top-4 -right-24 h-[90%] w-[30.25rem] rounded-t-lg shadow-2xl
+          className="absolute hidden sm:block top-4 -right-24 h-[50%] w-[30.25rem] rounded-t-lg shadow-2xl
         transition 
         group-hover:scale-[1.04]
         group-hover:-translate-x-3
