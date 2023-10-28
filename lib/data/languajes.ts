cbd2961af261bda5c7cafda9bbec2f8377960910
Pascual Eburi <pascualeburi@gmail.com>
2023-10-28 23:53:34 +0200

    wip

---
+++ lib/data/languajes.ts
@@ -0,0 +1,11 @@
+
+import Spain from "@/public/img/spain.png";
+import UK from "@/public/img/uk.png";
+import France from "@/public/img/france.png";
+
+
+export const CVLanguages = [
+    {name: 'Spanish', img: Spain, abbr: 'es' },
+    {name: 'English', img: UK, abbr: 'en' },
+    {name: 'French', img: France, abbr: 'fr' },
+] as const;
