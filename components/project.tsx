38640bdfcc7900e94c42f8c7947187660e718121
Pascual Eburi <pascualeburi@gmail.com>
2023-10-28 23:50:26 +0200

    wip

--- components/project.tsx
+++ components/project.tsx
@@ -1,65 +1,70 @@
-"use client"
-import type { ProjectProps } from "@/lib/types"
-import { motion, useScroll, useTransform } from "framer-motion"
-import Image from "next/image"
-import { useRef } from "react"
+"use client";
+import type { ProjectProps } from "@/lib/types";
+import { motion, useScroll, useTransform } from "framer-motion";
+import Image from "next/image";
+import { useRef } from "react";
 
+export default function Project({
+  title,
+  description,
+  tags,
+  imageUrl,
+  links,
+}: ProjectProps) {
+  const ref = useRef<HTMLLIElement>(null);
+  const { scrollYProgress } = useScroll({
+    target: ref,
+    offset: ["0 1", "1.33 1"],
+  });
 
-export default function Project ({ title, description, tags, imageUrl }: ProjectProps){
-    const ref = useRef<HTMLDivElement>(null)
-    const { scrollYProgress } = useScroll({
-        target: ref,
-        offset: ["0 1", "1.33 1"],
-      });
+  const scaleProgess = useTransform(scrollYProgress, [0, 1], [0.8, 1]);
+  const opacityProgess = useTransform(scrollYProgress, [0, 1], [0.6, 1]);
 
-     const scaleProgess = useTransform(scrollYProgress, [0, 1], [0.8, 1]);
-    const opacityProgess = useTransform(scrollYProgress, [0, 1], [0.6, 1]);
- 
   return (
-    <motion.div
-        ref = {ref}
-       style={{
+    <motion.li
+      ref={ref}
+      /*        style={{
         scale: scaleProgess,
         opacity: opacityProgess,
-      }} 
-      className="group mb-3 sm:mb-8 last:mb-0"
+      }} */
+      className="project"
     >
-      <section className="bg-gray-100 max-w-[42rem] border border-black/5 rounded-lg overflow-hidden sm:pr-8 relative  hover:bg-gray-200 transition sm:group-even:pl-8 dark:text-white dark:bg-white/10 dark:hover:bg-white/20">
-        <div className="pt-4 pb-7 px-5 sm:pl-10 sm:pr-2 sm:pt-10 sm:max-w-[40%] flex flex-col h-full sm:group-even:ml-[22rem]">
-          <h3 className="text-2xl font-semibold">{title}</h3>
-          <p className="mt-2 leading-relaxed text-gray-700 dark:text-white/70">
-            {description}
-          </p>
-          <ul className="flex flex-wrap mt-4 gap-2 sm:mt-auto">
+      <div className="project-content">
+        <div className="project-label">Featured Project</div>
+        <h4 className="project-title">{title}</h4>
+        <div className="project-details">
+          <p>{description}</p>
+          <ul>
             {tags.map((tag, index) => (
-              <li
-                className="bg-black/[0.7] px-3 py-1 text-[0.7rem] uppercase tracking-wider text-white rounded-full dark:text-white/70"
-                key={index}
-              >
-                {tag}
-              </li>
+              <li key={index}>{tag}</li>
             ))}
           </ul>
+          <div className="project-links mt-3">
+            {links.map((link, index) => (
+              <a
+                key={index}
+                href={link.url}
+                aria-label={`${link.name} Link`}
+                target="_blank"
+                title={link.name}
+                rel="noopener noreferrer"
+                className="text-2xl "
+              >
+                {link.icon}
+              </a>
+            ))}
+
+          </div>
         </div>
+      </div>
 
+      <div className="project-img">
         <Image
-          src={imageUrl}
           alt="Project I worked on"
-          quality={95}
-          className="absolute hidden sm:block top-4 -right-24 h-[50%] w-[30.25rem] rounded-t-lg shadow-2xl
-        transition 
-        group-hover:scale-[1.04]
-        group-hover:-translate-x-3
-        group-hover:translate-y-3
-        group-hover:-rotate-2
-
-        group-even:group-hover:translate-x-3
-        group-even:group-hover:translate-y-3
-        group-even:group-hover:rotate-2
-
-        group-even:right-[initial] group-even:-left-24"
+          src={imageUrl}
+          className="max-w-full w-full align-middle"
         />
-      </section>
-    </motion.div>
-    )
+      </div>
+    </motion.li>
+  );
 }
