d650e13ddf5f7df80e55fdc5b729b5a8db0cf344
Pascual Eburi <pascualeburi@gmail.com>
2023-10-30 23:47:45 +0100

    wip

--- components/sections/about.tsx
+++ components/sections/about.tsx
@@ -1,13 +1,12 @@
 /* eslint-disable react-hooks/exhaustive-deps */
-"use client"
+"use client";
 import { motion } from "framer-motion";
-import SectionHeading from "../section-heading"
+import SectionHeading from "../section-heading";
 import useSectionInView from "@/lib/hooks/useSectionInView";
 
 export default function About() {
   // check when component is in view to set active class to his link
-  const {ref} = useSectionInView({section: "About", threshold: 0.75});
-
+  const { ref } = useSectionInView({ section: "About", threshold: 0.75 });
 
   return (
     <motion.section
@@ -18,24 +17,30 @@ export default function About() {
       transition={{ delay: 0.175 }}
       id="about"
     >
-      <SectionHeading>About me</SectionHeading>
+      <SectionHeading>
+        <span className="text-indigo-400 block text-center text-sm">know</span>
+        About me
+      </SectionHeading>
       <p className="mb-3">
-        I had my first contact with programming when I was studying a degree in {" "}
-        <span className="font-medium">computer science and business management </span>. After that, I decided to pursue my passion for programming and web development {" "}
-        <span className="font-medium">by learning on my own</span>.{" "}
+        I had my first contact with programming when I was studying a degree in{" "}
+        <span className="font-medium">
+          computer science and business management{" "}
+        </span>
+        . After that, I decided to pursue my passion for programming and web
+        development <span className="font-medium">by learning on my own</span>.{" "}
         <span className="italic">My favorite part of programming</span> is the
         problem-solving aspect. I <span className="underline">love</span> the
-        feeling of finally figuring out a solution to a problem. And also, I've always loved the fact that I can take an idea in my head and turn it into something real through code. My core stack
-        is{" "}
+        feeling of finally figuring out a solution to a problem. And also, I've
+        always loved the fact that I can take an idea in my head and turn it
+        into something real through code. My core stack is{" "}
         <span className="font-medium">
           PHP, Python, React, Vue, MySQL and PostgreSQL.
         </span>
-         I am also familiar with TypeScript and Java. I am always looking to
+        I am also familiar with TypeScript and Java. I am always looking to
         learn new technologies. I am currently looking for a{" "}
-        <span className="font-medium">full-time position</span> as a web developer (Front o Back).
+        <span className="font-medium">full-time position</span> as a web
+        developer (Front o Back).
       </p>
-
-
     </motion.section>
-  )
+  );
 }
