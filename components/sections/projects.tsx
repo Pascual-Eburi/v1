fef5235c6d25ed4c20e0b834d750d6d24a0d738e
Pascual Eburi <pascualeburi@gmail.com>
2023-10-30 23:49:23 +0100

    wip

--- components/sections/projects.tsx
+++ components/sections/projects.tsx
@@ -1,26 +1,33 @@
 /* eslint-disable react-hooks/exhaustive-deps */
 "use client";
 
-import React from "react"
+import React from "react";
 import { projectsData } from "@/lib/data/projects";
 import SectionHeading from "../section-heading";
 import Project from "../project";
 import useSectionInView from "@/lib/hooks/useSectionInView";
 
-export default function Projects (){
-    // check when component is in view to set active class to his link
-    const {ref} = useSectionInView({section: "Projects", threshold: 0.45});
+export default function Projects() {
+  // check when component is in view to set active class to his link
+  const { ref } = useSectionInView({ section: "Projects", threshold: 0.45 });
 
-    return (
-        <section ref={ref} id="projects" className="scroll-mt-28 mb-28">
-            <SectionHeading> My Projects </SectionHeading>
-            <ul className="max-w-[1000px] mx-auto my-[50px] ">
-                {projectsData.map((project, index) => (
-                    <React.Fragment key={index}>
-                        <Project {...project} />
-                    </React.Fragment>
-                ))}
-            </ul>
-        </section>
-    );
+  return (
+    <section ref={ref} id="projects" className="scroll-mt-28 mb-28">
+      <SectionHeading>
+        <span className="text-[#CC7AFF] block text-center text-sm">
+          {" "}
+          Some of{" "}
+        </span>
+        My Projects
+      </SectionHeading>
+
+      <ul className="max-w-[1000px] mx-auto my-[50px] ">
+        {projectsData.map((project, index) => (
+          <React.Fragment key={index}>
+            <Project {...project} />
+          </React.Fragment>
+        ))}
+      </ul>
+    </section>
+  );
 }
