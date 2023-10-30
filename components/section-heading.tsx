150b0e0aa97381dd10dc236b9b7084ae298370c6
Pascual Eburi <pascualeburi@gmail.com>
2023-10-30 23:47:20 +0100

    wip

--- components/section-heading.tsx
+++ components/section-heading.tsx
@@ -1,9 +1,9 @@
 import type { SectionHeadingProps } from "@/lib/types";
 
-export default function SectionHeading ({ children } : SectionHeadingProps) {
-    return (
-        <h2 className="text-3xl font-medium capitalize mb-8 text-center">
-          {children}
-        </h2>
-      );
+export default function SectionHeading({ children }: SectionHeadingProps) {
+  return (
+    <h2 className="text-3xl font-bold capitalize mb-8 text-center">
+      {children}
+    </h2>
+  );
 }
