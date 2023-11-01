3e22b1e035848aa19ef0b99dc544976ab8b2b7e8
Pascual Eburi <pascualeburi@gmail.com>
2023-11-01 22:13:08 +0100

    Removed Experiences component

--- app/page.tsx
+++ app/page.tsx
@@ -1,22 +1,19 @@
 import Intro from "@/components/sections/intro";
 import SectionDivider from "@/components/section-divider";
-import About from "@/components/sections/about"
+import About from "@/components/sections/about";
 import Projects from "@/components/sections/projects";
 import Skills from "@/components/sections/skills";
-import Experiences from "@/components/sections/experiences";
 import Contact from "@/components/sections/contact";
-
 
 export default function Home() {
   return (
     <main className="flex flex-col items-center px-4">
-        <Intro />
-        <SectionDivider />
-        <About />
-        <Projects />
-         <Skills /> 
-        <Experiences />
-        <Contact />
+      <Intro />
+      <SectionDivider />
+      <About />
+      <Projects />
+      <Skills />
+      <Contact />
     </main>
-  )
+  );
 }
