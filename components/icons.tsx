af62c20b3c543b6696f294fb5e913e5261f6fd04
Pascual Eburi <pascualeburi@gmail.com>
2023-11-02 23:20:46 +0100

    Add folter svg icon

--- components/icons.tsx
+++ components/icons.tsx
@@ -98,3 +98,20 @@ export function FrenchFlagSvg() {
     </svg>
   );
 }
+
+export const IconFolder = ({ className }: { className: string }) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    role="img"
+    viewBox="0 0 24 24"
+    fill="none"
+    stroke="currentColor"
+    strokeWidth="1"
+    strokeLinecap="round"
+    strokeLinejoin="round"
+    className={className}
+  >
+    <title>Folder</title>
+    <path d="M22 19a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h5l2 3h9a2 2 0 0 1 2 2z"></path>
+  </svg>
+);
