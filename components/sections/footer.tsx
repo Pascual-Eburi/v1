1387cdb7fd0bced0f4dd2b312c131ab1eb733ef8
Pascual Eburi <pascualeburi@gmail.com>
2023-11-02 03:18:50 +0100

    wip

--- components/sections/footer.tsx
+++ components/sections/footer.tsx
@@ -1,19 +1,23 @@
-import {BsFillHeartFill} from "react-icons/bs"
+import { BsFillHeartFill } from "react-icons/bs";
 
 export default function Footer() {
   const year = new Date().getFullYear();
   return (
     <footer className="mb-10 px-4 text-center text-gray-500">
       <small className="mb-2 block text-xs">
-        &copy; { year} Pascual Eburi. All rights reserved.
+        &copy; {year} Pascual Eburi. All rights reserved.
       </small>
       <p className="text-xs text-center">
-        <span className="font-semibold">About this website:</span> 
-        <span className="flex w-full align-center justify-center  text-center"> 
-            Made with <span className="mx-2"><BsFillHeartFill color="#f87171"/> </span> 
-             & built with React & Next.js, TypeScript, Tailwind CSS, Framer Motion, Vercel hosting.
+        <span className="font-semibold">About this website:</span>
+        <span className="flex w-full align-center justify-center  text-center">
+          Made with{" "}
+          <span className="mx-2">
+            <BsFillHeartFill color="#f87171" />{" "}
+          </span>
+          & built with React & Next.js, TypeScript, Tailwind CSS, Framer Motion,
+          Vercel hosting.
         </span>
       </p>
     </footer>
-  )
+  );
 }
