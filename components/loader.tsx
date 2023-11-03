202e4eeb65c753d339c32eac7c798c0c57be8fc5
Pascual Eburi <pascualeburi@gmail.com>
2023-11-03 23:07:55 +0100

    Implement loader component

---
+++ components/loader.tsx
@@ -0,0 +1,15 @@
+export default function Loader() {
+  return (
+    <div className="fixed h-screen w-screen top-0 bottom-0 left-0 right-0 bg-black !z-50 block text-center text-white font-bold text-xl">
+      <div className="relative w-screen h-screen">
+        <div className="frame">
+          <div className="center">
+            <div className="dot-1"></div>
+            <div className="dot-2"></div>
+            <div className="dot-3"></div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
