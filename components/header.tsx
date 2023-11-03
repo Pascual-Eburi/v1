b3c46de0f93ff19b22ed8e003457605e004b0f57
Pascual Eburi <pascualeburi@gmail.com>
2023-11-03 22:57:10 +0100

    Add activeSection class to active link

--- components/header.tsx
+++ components/header.tsx
@@ -11,7 +11,7 @@ export default function Header() {
   const { activeSection, setActiveSection, setTimeOfLastClick } =
     useActiveSectionContext();
   return (
-    <header className="z-[999] relative">
+    <header className="z-[39] relative">
       <motion.div
         className="fixed top-0 left-1/2 h-[4.5rem] w-full rounded-none border border-white border-opacity-40 bg-white bg-opacity-80 shadow-lg shadow-black/[0.03] backdrop-blur-[0.5rem] sm:top-6 sm:h-[3.25rem] sm:w-[36rem] sm:rounded-full dark:bg-gray-950 dark:border-black/40 dark:bg-opacity-75"
         initial={{ y: 100, x: "-50%", opacity: 0 }}
@@ -30,7 +30,8 @@ export default function Header() {
                 className={clsx(
                   "flex w-full items-center justify-center px-3 py-3 hover:text-gray-950 transition dark:text-gray-500 dark:hover:text-gray-300",
                   {
-                    "!text-gray-100/80 font-bold": link.name === activeSection,
+                    "!text-gray-100/80 font-bold activeSection":
+                      link.name === activeSection,
                   }
                 )}
                 href={link.hash}
