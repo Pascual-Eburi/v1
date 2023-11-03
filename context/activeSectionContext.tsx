19a06e20d17d2987c4d9e58962fcc4c6c0687019
Pascual Eburi <pascualeburi@gmail.com>
2023-11-03 23:04:35 +0100

    wip

--- context/activeSectionContext.tsx
+++ context/activeSectionContext.tsx
@@ -1,28 +1,55 @@
 "use client";
 
-import React, {useState, createContext, useContext} from "react"
-import type { SectionName, ActiveSectionContextProviderProps, ActiveSectionContextType } from "@/lib/types";
+import React, { useState, createContext, useContext, useEffect } from "react";
+import type {
+  SectionName,
+  ActiveSectionContextProviderProps,
+  ActiveSectionContextType,
+} from "@/lib/types";
 
+export const ActiveSectionContext =
+  createContext<ActiveSectionContextType | null>(null);
 
-export const ActiveSectionContext = createContext<ActiveSectionContextType | null>(null);
+export default function ActiveSectionContextProvider({
+  children,
+}: ActiveSectionContextProviderProps) {
+  const [activeSection, setActiveSection] = useState<SectionName>("Home");
+  const [timeOfLastClick, setTimeOfLastClick] = useState(0);
 
-export default function ActiveSectionContextProvider({ children}:ActiveSectionContextProviderProps){
-    const [activeSection, setActiveSection] = useState<SectionName>('Home');
-    const [timeOfLastClick, setTimeOfLastClick] = useState(0);
+  /* useEffect(() => {
+    const storedSectionActive = localStorage.getItem("activeSection") ?? "Home";
+    if (storedSectionActive !== activeSection) {
+      window.localStorage.setItem("activeSection", activeSection);
+      window.history.pushState(
+        null,
+        "",
+        `#${storedSectionActive.toLowerCase()}`
+      );
+    }
+    console.log("Active", activeSection);
+  }, [activeSection]); */
 
-    return <ActiveSectionContext.Provider value={
-        {activeSection, setActiveSection, timeOfLastClick, setTimeOfLastClick}}>
-        {children}
+  return (
+    <ActiveSectionContext.Provider
+      value={{
+        activeSection,
+        setActiveSection,
+        timeOfLastClick,
+        setTimeOfLastClick,
+      }}
+    >
+      {children}
     </ActiveSectionContext.Provider>
+  );
 }
 
+export function useActiveSectionContext() {
+  const context = useContext(ActiveSectionContext);
+  if (context === null) {
+    throw new Error(
+      "useActiveSectionContext must be used within an ActiveSectionContextProvider"
+    );
+  }
 
-export function useActiveSectionContext(){
-    const context = useContext(ActiveSectionContext);
-    if (context === null){
-        throw new Error ("useActiveSectionContext must be used within an ActiveSectionContextProvider")
-    }
-
-    return context;
-
+  return context;
 }
