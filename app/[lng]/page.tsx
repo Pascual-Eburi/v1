6232f262e5aa977ac71381d9e8c47ae4de208a60
Pascual Eburi <pascualeburi@gmail.com>
2023-11-04 23:59:49 +0100

    wip

---
+++ app/[lng]/page.tsx
@@ -0,0 +1,49 @@
+"use client";
+import Intro from "@/components/sections/intro";
+import SectionDivider from "@/components/section-divider";
+import About from "@/components/sections/about";
+import Projects from "@/components/sections/projects";
+import Skills from "@/components/sections/skills";
+import Contact from "@/components/sections/contact";
+import { useEffect, useState } from "react";
+import Loader from "@/components/loader";
+import type { PagePropsType } from "@/lib/types";
+
+import { useTranslation } from "react-i18next";
+import RootLayout from "./layout";
+
+export default function Home({ params: { lng } }: PagePropsType) {
+  const [mounted, setMounted] = useState(false);
+
+  useEffect(() => {
+    const hash = window.location.hash ?? "#home";
+    /*     let activeSection = document.querySelector(
+      `[href="${hash}"]`
+    ) as HTMLAnchorElement;
+
+    if (!activeSection) {
+      activeSection = document.querySelector(
+        ".activeSection"
+      ) as HTMLAnchorElement;
+    }
+
+    activeSection.click(); */
+    setMounted(true);
+  }, []);
+
+  if (!mounted) {
+    return <Loader />;
+  }
+  return (
+    <RootLayout>
+      <main className="flex flex-col items-center px-4">
+        <Intro />
+        <SectionDivider />
+        <About />
+        <Projects />
+        <Skills />
+        <Contact />
+      </main>
+    </RootLayout>
+  );
+}
