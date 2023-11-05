f45bcb764931c62fee601f74b082ea7378be706b
Pascual Eburi <pascualeburi@gmail.com>
2023-11-05 23:55:59 +0100

    Implemented LanguageSwitch with internationalization (i18n)

--- components/language-swith.tsx
+++ components/language-swith.tsx
@@ -3,17 +3,18 @@
 import { Fragment, useEffect, useState } from "react";
 import { Menu, Transition } from "@headlessui/react";
 import Image from "next/image";
-import { CVLanguages } from "@/lib/data/languajes";
+import { Languages } from "@/lib/data/languajes";
 import type { Language } from "@/lib/types";
+import Link from "next/link";
+import { useLanguageContext } from "@/context/languageContext";
 
 export default function LanguageSwith() {
+  const { lng, t } = useLanguageContext("languages");
   const [mounted, setMounted] = useState(false);
-  const browserLanguage =
-    typeof navigator !== "undefined" ? navigator.language.split("-")[0] : "en";
 
-  const fallBackLanguage = CVLanguages[0] as Language;
-  const initialLanguage: Language | undefined = CVLanguages.find(
-    (option) => option.abbr === browserLanguage
+  const fallBackLanguage = Languages[0] as Language;
+  const initialLanguage: Language | undefined = Languages.find(
+    (option) => option.abbr === lng
   );
 
   const [language, setLanguage] = useState<Language>(
@@ -52,17 +53,17 @@ export default function LanguageSwith() {
           leaveFrom="transform opacity-100 scale-100"
           leaveTo="transform opacity-0 scale-95"
         >
-          <Menu.Items className="absolute right-0 -top-[7.5rem] !z-10 mt-2 w-max origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none dark:bg-gray-700">
+          <Menu.Items className="absolute right-0 -top-[5.2rem] !z-10 mt-2 w-max origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none dark:bg-gray-700">
             <div className="py-1">
-              {CVLanguages.map((lang, index) => (
+              {Languages.filter((l) => lng !== l.abbr).map((lang, index) => (
                 <Fragment key={index}>
                   <Menu.Item>
                     {({ active }) => (
-                      <a
+                      <Link
                         onClick={() => {
                           setLanguage(lang);
                         }}
-                        href="#"
+                        href={`/${lang.abbr}`}
                         className={`flex items-center hover:bg-gray-100 gap-2 px-4 py-2 text-xs text-gray-700 dark:text-white/80 dark:hover:bg-white/10 dark:focus:bg-white/10 ${
                           lang.name === language.name && "bg-gray-800 font-bold"
                         }`}
@@ -72,12 +73,11 @@ export default function LanguageSwith() {
                           height="90"
                           src={lang.img}
                           alt={lang.name}
-                          className={`w-[1rem] h-[1rem] object-cover  
-                          ${lang.name !== language.name && "grayscale-[80%]"}`}
+                          className={`w-[1rem] h-[1rem] object-cover`}
                         />
 
-                        <span>{lang.name} </span>
-                      </a>
+                        <span>{t(lang.abbr)} </span>
+                      </Link>
                     )}
                   </Menu.Item>
                 </Fragment>
