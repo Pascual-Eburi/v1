674f6d16703e12e60858adb96d441d18383381e5
Pascual Eburi <pascualeburi@gmail.com>
2023-11-25 20:18:57 +0100

    Added translations for footer component

--- components/sections/footer.tsx
+++ components/sections/footer.tsx
@@ -1,21 +1,21 @@
+"use client";
+import { useLanguageContext } from "@/context/languageContext";
 import { BsFillHeartFill } from "react-icons/bs";
 
 export default function Footer() {
+  const { t } = useLanguageContext("footer");
   const year = new Date().getFullYear();
   return (
     <footer className="mb-10 px-4 text-center text-gray-500">
-      <small className="mb-2 block text-xs">
-        &copy; {year} Pascual Eburi. All rights reserved.
-      </small>
+      <small className="mb-2 block text-xs">&copy; {year} Pascual Eburi.</small>
       <p className="text-xs text-center">
-        <span className="font-semibold">About this website:</span>
+        <span className="font-semibold">{t("about-site")}</span>
         <span className="flex w-full align-center justify-center  text-center">
-          Made with{" "}
+          {t("made-with")}{" "}
           <span className="mx-2">
             <BsFillHeartFill color="#f87171" />{" "}
           </span>
-          & built with React & Next.js, TypeScript, Tailwind CSS, Framer Motion,
-          Vercel hosting.
+          {t("build-with")}
         </span>
       </p>
     </footer>
