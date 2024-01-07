8fd756ba29d1a7fc7f987a65afcae7d0bc7c79d9
Pascual Eburi <pascualeburi@gmail.com>
2024-01-07 14:35:23 +0100

    fix

--- components/sections/intro.tsx
+++ components/sections/intro.tsx
@@ -98,6 +98,8 @@ export default function Intro() {
             className="bg-white p-4 text-gray-700 hover:text-gray-950 flex items-center gap-2 rounded-full focus:scale-[1.15] hover:scale-[1.15] active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10 dark:text-white/60"
             href="https://www.linkedin.com/in/pascualeburi/"
             target="_blank"
+            aria-label="LinkedIn Profile"
+            id="LinkedInProfile"
           >
             <BsLinkedin />
           </a>
@@ -106,6 +108,7 @@ export default function Intro() {
             className="bg-white p-4 text-gray-700 flex items-center gap-2 text-[1.35rem] rounded-full focus:scale-[1.15] hover:scale-[1.15] hover:text-gray-950 active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10 dark:text-white/60"
             href="https://github.com/Pascual-Eburi"
             target="_blank"
+            aria-label="GitHub Profile"
           >
             <FaGithubSquare />
           </a>
